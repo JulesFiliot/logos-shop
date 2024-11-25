e3ae6eea402af443df05f4eb388374ddedaa1444
JulesFILIOT <dev@julesfiliot.com>
2024-11-26 00:24:01 +0100

    refactor(Grid): minor code readability and robustness enhancements for Grid component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,18 +1,20 @@
 import styled from '@emotion/styled'
 import { PropsWithChildren } from 'react'
 
-type GridProps = {
+export type GridProps = {
   columns?: number
   gap?: string
+  width?: string
 }
 
 export const Grid = ({
   columns = 2,
   gap = '8px',
   children,
+  width = '100%',
 }: PropsWithChildren & GridProps) => {
   return (
-    <GridContainer columns={columns} gap={gap}>
+    <GridContainer columns={columns} gap={gap} width={width}>
       {children}
     </GridContainer>
   )
@@ -22,32 +24,29 @@ const GridContainer = styled.div<GridProps>`
   display: grid;
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
   gap: ${(props) => props.gap};
-  width: 100%;
+  width: ${(props) => props.width};
   position: relative;
   margin: 0 auto;
 
   ${(props) =>
     props.gap &&
     ['0', '0px'].includes(props.gap) &&
-    `
-    gap: 0;
-    
+    `    
     > * {
       margin-right: -1px;
       margin-bottom: -1px;
       position: relative;
 
-      &:not(:nth-child(${props.columns}n)) {
+      &:not(:nth-of-type(${props.columns}n)) {
         border-right: none;
       }
 
-      &:not(:nth-last-child(-n+${props.columns})) {
+      &:not(:nth-last-of-type(-n+${props.columns})) {
         border-bottom: none;
       }
 
-      &:last-child:not(:nth-child(${props.columns}n)) {
+      &:last-child:not(:nth-of-type(${props.columns}n)) {
         border-right: 1px solid rgb(var(--lsd-border-primary));
-        width: calc(100% + 1px);
       }
     }
   `}
